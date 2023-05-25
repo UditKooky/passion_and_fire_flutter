f7adb99c2e144a96ba2b525f57031d4a7c33c976
Ankit Bansal <134496607+ankitKooky@users.noreply.github.com>
2023-05-25 11:52:48 +0530

    Splash and declare color code constant

--- lib/Screens/splash_screen.dart
+++ lib/Screens/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:passion_and_fire_flutter/utils/colors.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -8,8 +9,19 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  var height,width,size;
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+     size = MediaQuery.of(context).size;
+    height = size.height;
+    width = size.width;
+    return Scaffold(
+      backgroundColor: primaryColor,
+      body: Container(
+        height: height,
+        width: width,
+        child: Image.asset("assets/images/appLogo.png"),
+      ),
+    );
   }
 }
